a82b743a8d9730c35ac6788b9eea54314de311d7
Peter Dowell <pete.rocks@gmail.com>
2020-05-23 22:51:50 +0100

    Addd Tests

---
+++ test/test_io.py
@@ -0,0 +1,14 @@
+import os
+from summarise.io import pdf_to_text
+from summarise.io import pdf_to_text_file
+
+
+root = os.path.dirname(__file__)
+
+text = pdf_to_text( os.path.join( root, 'resources', 'samples', 'Document2.pdf' ) )
+
+print( text )
+
+filenames = pdf_to_text_file( os.path.join( root, 'resources', 'samples' ) )
+
+print(filenames)
